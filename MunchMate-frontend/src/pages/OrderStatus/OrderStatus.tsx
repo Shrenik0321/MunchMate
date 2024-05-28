ed57028747a689710d55ed271d37eca5c6db8f6a
Shrenik Deep <s.shrenikdeep@gmail.com>
2024-05-28 22:36:59 +0530

    UI Update

--- MunchMate-frontend/src/pages/OrderStatus/OrderStatus.tsx
+++ MunchMate-frontend/src/pages/OrderStatus/OrderStatus.tsx
@@ -1,7 +1,17 @@
+import { Progress } from "@/components/ui/progress";
+import React from "react";
+
 const OrderStatus = () => {
+  const [progress, setProgress] = React.useState(13);
+
+  React.useEffect(() => {
+    const timer = setTimeout(() => setProgress(66), 500);
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
-    <div>
-      <div className="flex justify-between m-10">
+    <div className="m-10">
+      <div className="flex justify-between">
         <div>
           <p className="text-3xl font-bold">
             Order Status: Awaiting Restaurant Confirmation
@@ -9,6 +19,26 @@ const OrderStatus = () => {
         </div>
         <div>
           <p className="text-3xl font-bold">Expected by : 15:12</p>
+        </div>
+      </div>
+
+      <div className="my-10">
+        <Progress value={progress} className="w-full" />
+      </div>
+
+      <div className="grid grid-cols-2 gap-4">
+        <div className="...">
+          <p className="text-3xl font-bold">Delivery Details</p>
+        </div>
+
+        <div>
+          <div className="size-10/12">
+            <img
+              src="/src/assets/Group 8.png"
+              alt="PDF Image"
+              className="w-full h-full object-contain"
+            />
+          </div>
         </div>
       </div>
     </div>
