461465057ff7d3e84b458a44d1698bdf3cf60cca
Shrenik Deep <s.shrenikdeep@gmail.com>
2024-07-23 22:27:14 +0530

    sidebar ui update

--- MunchMate-frontend/src/components/AdminSidebar/AdminSidebar.tsx
+++ MunchMate-frontend/src/components/AdminSidebar/AdminSidebar.tsx
@@ -1,4 +1,11 @@
-import { Home, Utensils, NotepadText } from "lucide-react";
+import { useState } from "react";
+import {
+  Home,
+  Utensils,
+  NotepadText,
+  ChevronsLeft,
+  ChevronsRight,
+} from "lucide-react";
 import { useNavigate } from "react-router-dom";
 import {
   Accordion,
@@ -8,7 +15,6 @@ import {
 } from "@/components/ui/accordion";
 import { Button } from "@/components/ui/button";
 
-// Custom CSS to remove the underline and prevent icon rotation
 const customStyles = `
   .accordion-item, .accordion-trigger {
     border-bottom: none !important;
@@ -24,10 +30,17 @@ const customStyles = `
 
 const AdminSidebar = () => {
   const navigate = useNavigate();
+  const [isMinimized, setIsMinimized] = useState(false);
 
   const menuItems = [
     { label: "Overview", icon: Home, path: "/admin/overview" },
     { label: "Orders", icon: NotepadText, path: "/admin/orders" },
+  ];
+
+  const menuItemsMinimised = [
+    { label: "Overview", icon: Home, path: "/admin/overview" },
+    { label: "Orders", icon: NotepadText, path: "/admin/orders" },
+    { label: "Restaurants", icon: Utensils, path: "/admin/restaurants" },
   ];
 
   const restaurantItems = [
@@ -36,48 +49,88 @@ const AdminSidebar = () => {
   ];
 
   return (
-    <div className="flex flex-col h-full p-6 bg-white text-black">
+    <div>
       <style>{customStyles}</style>
-      <h2 className="text-2xl font-bold pb-6">Manage Restaurant</h2>
-      <div className="space-y-4">
-        {menuItems.map((item) => (
-          <Button
-            key={item.label}
-            variant="ghost"
-            className="flex items-center space-x-3 justify-start w-full text-left py-2 px-4 hover:bg-gray-200 transition-colors duration-200 rounded-lg"
-            onClick={() => navigate(item.path)}
-          >
-            <item.icon className="w-5 h-5" color="#f97316" />
-            <span>{item.label}</span>
-          </Button>
-        ))}
+      {isMinimized ? (
+        <div className="flex flex-col h-full bg-white text-black transition-width duration-200 w-16">
+          <div className="flex justify-between mx-2 my-4">
+            <Button
+              variant="ghost"
+              onClick={() => setIsMinimized(!isMinimized)}
+            >
+              <ChevronsRight className="w-5 h-5" color="#f97316" />
+            </Button>
+          </div>
 
-        <Accordion type="single" collapsible>
-          <AccordionItem value="restaurants" className="accordion-item">
-            <AccordionTrigger className="flex items-center justify-between w-full text-left py-2 px-4 hover:bg-gray-200 transition-colors duration-200 rounded-lg no-rotate accordion-trigger">
-              <div className="flex items-center space-x-3">
-                <Utensils
-                  className="w-5 h-5 accordion-trigger-icon"
-                  color="#f97316"
-                />
-                <span>Restaurants</span>
-              </div>
-            </AccordionTrigger>
-            <AccordionContent className="pl-6 space-y-2">
-              {restaurantItems.map((item) => (
-                <Button
-                  key={item.label}
-                  variant="ghost"
-                  className="flex items-center space-x-2 justify-start w-full text-left py-2 px-4 hover:bg-gray-200 transition-colors duration-200 rounded-lg"
-                  onClick={() => navigate(item.path)}
-                >
-                  <span>{item.label}</span>
-                </Button>
-              ))}
-            </AccordionContent>
-          </AccordionItem>
-        </Accordion>
-      </div>
+          <div className="space-y-4">
+            {menuItemsMinimised.map((item) => (
+              <Button
+                key={item.label}
+                variant="ghost"
+                className="flex items-center space-x-3 w-full text-left py-2 px-4 hover:bg-gray-200 transition-colors duration-200 rounded-lg justify-center"
+                onClick={() => navigate(item.path)}
+              >
+                <item.icon className="w-5 h-5" color="#f97316" />
+              </Button>
+            ))}
+          </div>
+        </div>
+      ) : (
+        <div className="flex flex-col h-full bg-white text-black transition-width duration-200 w-64">
+          <div className="flex justify-between mx-2 my-4">
+            <div>
+              <h2 className="text-xl font-bold">Manage Restaurant</h2>
+            </div>
+
+            <Button
+              variant="ghost"
+              onClick={() => setIsMinimized(!isMinimized)}
+            >
+              <ChevronsLeft className="w-5 h-5" color="#f97316" />
+            </Button>
+          </div>
+
+          <div className="space-y-4">
+            {" "}
+            {menuItems.map((item) => (
+              <Button
+                key={item.label}
+                variant="ghost"
+                className="flex items-center space-x-3 justify-start w-full text-left py-2 px-4 hover:bg-gray-200 transition-colors duration-200 rounded-lg"
+                onClick={() => navigate(item.path)}
+              >
+                <item.icon className="w-5 h-5" color="#f97316" />
+                {<span>{item.label}</span>}
+              </Button>
+            ))}
+            <Accordion type="single" collapsible>
+              <AccordionItem value="restaurants" className="accordion-item">
+                <AccordionTrigger className="flex items-center justify-between w-full text-left py-2 px-4 hover:bg-gray-200 transition-colors duration-200 rounded-lg no-rotate accordion-trigger">
+                  <div className="flex items-center space-x-3">
+                    <Utensils
+                      className="w-5 h-5 accordion-trigger-icon"
+                      color="#f97316"
+                    />
+                    {!isMinimized && <span>Restaurants</span>}
+                  </div>
+                </AccordionTrigger>
+                <AccordionContent className="pl-6 space-y-2">
+                  {restaurantItems.map((item) => (
+                    <Button
+                      key={item.label}
+                      variant="ghost"
+                      className="flex items-center space-x-2 justify-start w-full text-left py-2 px-4 hover:bg-gray-200 transition-colors duration-200 rounded-lg"
+                      onClick={() => navigate(item.path)}
+                    >
+                      <span>{item.label}</span>
+                    </Button>
+                  ))}
+                </AccordionContent>
+              </AccordionItem>
+            </Accordion>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
